2eb2082e2b76c66dac39bb05e6f789c576907e13
Dyf-Tfh <n.zhao@hotmail.fr>
2018-07-25 13:41:28 +0200

    done

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+5.times do |index|
+  wof = Cour.create(title: "Cour n#{index + 1}", description: "descriptionn#{index + 1}")
+  3.times do |index_lesson|
+    Lesson.create(title: "Lesson#{index_lesson +1}", content: "Cours n#{index + 1}, lesson#{index_lesson + 1}", cour_id: wof.id)
+  end 
+end
+
